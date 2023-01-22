a3e3174f006ffc0c47b7861611d65347f89ff156
Savan <sawanpatel2508@gmail.com>
2023-01-22 12:31:56 -0500

    Included the optparse method to allow user to include the arguments directly while executing the program.

--- Mac_changer.py
+++ Mac_changer.py
@@ -4,10 +4,23 @@ This program lets you change your mac address for your desired device to
 your desire MAC address
 """
 import subprocess
+import optparse
 
-subprocess.call("ifconfig", shell=True)
-interface = input('Please include the device: ')
-mac_address = input('Please enter desired mac Address: ')
+parser = optparse.OptionParser()
+
+parser.add_option("-i","--interface", dest='interface', help="Interface to change its mac address")
+parser.add_option("-m","--mac", dest='mac_address', help="Please enter desired MAC address for to change for the "
+                                                         "interface")
+
+(options, arguments) = parser.parse_args()
+if options.interface and options.mac_address:
+    interface = options.interface
+    mac_address = options.mac_address
+
+else:
+    subprocess.call("ifconfig", shell=True)
+    interface = input('Please include the device: ')
+    mac_address = input('Please enter desired mac Address: ')
 
 subprocess.call("ifconfig " + interface + " down", shell=True)
 subprocess.call("ifconfig " + interface + " hw ether " + mac_address, shell=True)
